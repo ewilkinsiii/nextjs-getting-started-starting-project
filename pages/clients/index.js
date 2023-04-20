c65b5119ff3ae6ee0a8c5805e3045ca629a607e9
railshive <ewilkins@railshive.com>
2023-04-20 10:20:05 -0500

    added dynamic links

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,9 +1,27 @@
+import Link from "next/link";
 const ClientPage = () => {
-    return(
-        <div>
-            <h1>The Clients Page</h1>
-        </div>
-    );
-}
+  const clients = [
+    {
+      id: "max",
+      name: "Maximilian",
+    },
+    {
+      id: "manu",
+      name: "Manual",
+    },
+  ];
+  return (
+    <div>
+      <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`clients/${client.id}`}> {client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default ClientPage
+export default ClientPage;
